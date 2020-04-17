6542bd53f85c0f6e16f288ff156cbbe9d4c10ad1
Roy Ortega <royeo91@gmail.com>
2020-04-16 18:13:04 -0700

    Listbox
    Listbox now constructed upon initialization

--- main_app_controller.py
+++ main_app_controller.py
@@ -8,6 +8,7 @@ from classlist_window import ClasslistWindow
 from add_song_window import AddSongWindow
 import eyed3
 import os
+import vlc
 
 
 class MainAppController(tk.Frame):
@@ -18,6 +19,7 @@ class MainAppController(tk.Frame):
         tk.Frame.__init__(self, parent)
         self._root_win = tk.Toplevel()
         self._player = PlayerWindow(self._root_win, self)
+        self.listbox_callback()
 
     # 6: define callback functions
     def clear_callback(self):
@@ -32,17 +34,6 @@ class MainAppController(tk.Frame):
     def quit_callback(self):
         """ Exit the application. """
         self.master.quit()
-
-    """ Don't need this """
-    # def rand_callback(self):
-    #  """ Random select a name and display on GUI. """
-    #  response = requests.get("http://localhost:5000/student/random")
-    #  student = Student.from_dict(response.json())
-    #  if response.status_code == 200:
-    #     full_name = f"{student.first_name} {student.last_name}"
-    #     self._chooser.display_student_name(full_name)
-    #  elif response.status_code == 404:
-    #     messagebox.showinfo(title='Random', message="No names in DB")
 
     def add_callback(self):
         """ Add a new student name to the file. """
@@ -64,54 +55,10 @@ class MainAppController(tk.Frame):
         if response.status_code == 400:
             msg_str = 'Song already exists'
             messagebox.showinfo(title='Add Song', message=msg_str)
-        # response_names = requests.get("http://localhost:5000/student/names")
-        # name_list = [f'{s["first_name"]} {s["last_name"]}' for s in response_names.json()]
-        # self._class.set_names(name_list)
+
+        self.listbox_callback()
         self._add_song._close_cb()
         return
-
-    """ Don't need this? Unless we refactor it to make it open file on click """
-    # def openfile(self):
-    #     """ Load all the names from the file """
-    #     selected_file = askopenfilename(initialdir='.')
-    #     if selected_file:
-    #         self.file_name = selected_file
-    #         num_added = 0
-    #         not_added = []
-    #         with open(self.file_name, 'r') as csvfile:
-    #             csv_reader = csv.reader(csvfile, delimiter=',')
-    #             for row in csv_reader:
-    #                 data = {'student_id': row[0],
-    #                         'first_name': row[1],
-    #                         'last_name': row[2]}
-    #                 response = requests.post("http://localhost:5000/student",
-    #                              json=data)
-    #                 if response.status_code == 200:
-    #                     num_added += 1
-    #                 else:
-    #                     not_added.append(' '.join(row))
-    #         msg = f'{num_added} names added to DB.'
-    #         if len(not_added) > 0:
-    #             not_added = '\n'.join(not_added)
-    #             msg += '\n' + f'The following names were not added:'
-    #             msg += '\n' + not_added
-    #         messagebox.showinfo(title='Load Names', message=msg)
-
-
-    def classlist_popup(self):
-        """ Show Classlist Popup Window """
-        self._class_win = tk.Toplevel()
-        self._class = ClasslistWindow(self._class_win, self._close_classlist_popup, self.delete_callback, self)
-        response = requests.get("http://localhost:5000/student/names")
-
-        name_list = [f'{s["first_name"]} {s["last_name"]}' for s in response.json()]
-        self._class.set_names(name_list)
-
-
-    def _close_classlist_popup(self):
-        """ Close Classlist Popup """
-
-        self._class_win.destroy()
 
 
     def add_song_popup(self):
@@ -128,23 +75,35 @@ class MainAppController(tk.Frame):
 
     def delete_callback(self):
         """ Deletes a student from the list and db"""
-        student_listbox = self._class.name_listbox
-        item = student_listbox.curselection()
+        song_listbox = self._player.song_listbox
+        item = song_listbox.curselection()
         index = item[0]
+        id = item[1]
+        print(index, id)
 
-        student_name = self._class.name_listbox.get(index)
+        song_info = self._player.song_listbox.get(index)
 
-        response = requests.delete("http://localhost:5000/student/" + student_name)
+        response = requests.delete("http://localhost:5000/ong/" + id)
 
         if response.status_code == 200:
-            msg_str = f'{student_name} deleted from the database'
+            msg_str = f'{song_info} deleted from the database'
             messagebox.showinfo(title='Add Student', message=msg_str)
 
-        response_names = requests.get("http://localhost:5000/student/names")
-        name_list = [f'{s["first_name"]} {s["last_name"]}' for s in response_names.json()]
-        self._class.set_names(name_list)
+        self.listbox_callback()
 
-    """ Use this to start the add song """
+    def listbox_callback(self):
+        """ Gets a list of all songs and """
+        response_names = requests.get("http://localhost:5000/song/songs")
+        song_list = [f'{s["title"]} {s["artist"]}' for s in response_names.json()]
+        song_listbox = self._player.song_listbox
+        song_listbox.delete(0, tk.END)
+        for song in song_list:
+            song_listbox.insert(tk.END, song)
+
+
+    def play_callback(self):
+        pass
+
     def load(self, song_url):
         """ Loads a song by the url"""
         split_path = os.path.split(song_url['path_name'])
@@ -164,6 +123,7 @@ class MainAppController(tk.Frame):
                     }
 
         return song_info
+
 
 
 
